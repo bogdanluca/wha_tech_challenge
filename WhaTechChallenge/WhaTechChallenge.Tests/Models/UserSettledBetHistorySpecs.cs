8b1202fdfa1ffe3a4d91c3a248e5f1680cd9f408
bogdy luca <bogdan_saca@yahoo.com>
2016-01-19 21:55:37 +1100

    Added HasUnusualWinningRate flag.

---
+++ WhaTechChallenge/WhaTechChallenge.Tests/Models/UserSettledBetHistorySpecs.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using WhaTechChallenge.Models;
+
+namespace WhaTechChallenge.Tests.Models
+{
+    [TestFixture]
+    public class UserSettledBetHistorySpecs
+    {
+        public IList<SettledBetHistoryItem> CreateBetHistoryWithUnusualWinningRate()
+        {
+            return new List<SettledBetHistoryItem>
+            {
+                new SettledBetHistoryItem {CustomerID = 1, Stake = 10, Win = 20},
+                new SettledBetHistoryItem {CustomerID = 1, Stake = 20, Win = 0},
+                new SettledBetHistoryItem {CustomerID = 1, Stake = 30, Win = 40}
+            };
+        }
+
+        public IList<SettledBetHistoryItem> CreateBetHistoryWithNormalWinningRate()
+        {
+            return new List<SettledBetHistoryItem>
+            {
+                new SettledBetHistoryItem {CustomerID = 1, Stake = 10, Win = 20},
+                new SettledBetHistoryItem {CustomerID = 1, Stake = 20, Win = 0},
+                new SettledBetHistoryItem {CustomerID = 1, Stake = 50, Win = 0},
+                new SettledBetHistoryItem {CustomerID = 1, Stake = 30, Win = 40}
+            };
+        }
+
+        [Test]
+        public void Should_have_unusual_wining_rate_flag_set_to_false_if_winning_rate_is_lower_than_60_percent()
+        {
+            var userBetHistory = new UserSettledBetHistory(CreateBetHistoryWithNormalWinningRate());
+
+            Assert.That(userBetHistory.HasUnusualWinningRate, Is.False);
+        }
+
+        [Test]
+        public void Should_have_unusual_wining_rate_flag_set_to_true_if_winning_rate_is_greater_than_60_percent()
+        {
+            var userBetHistory = new UserSettledBetHistory(CreateBetHistoryWithUnusualWinningRate());
+
+            Assert.That(userBetHistory.HasUnusualWinningRate, Is.True);
+        }
+    }
+}
--- WhaTechChallenge/WhaTechChallenge/Models/UserSettledBetHistory.cs
+++ WhaTechChallenge/WhaTechChallenge/Models/UserSettledBetHistory.cs
@@ -5,16 +5,31 @@ namespace WhaTechChallenge.Models
 {
     public class UserSettledBetHistory
     {
-        public UserSettledBetHistory(IEnumerable<SettledBetHistoryItem> bets)
+        private const decimal UnusualWinRateThreshold = 0.6m;
+
+        public UserSettledBetHistory(IList<SettledBetHistoryItem> bets)
         {
             Bets = bets ?? new List<SettledBetHistoryItem>();
         }
 
-        public IEnumerable<SettledBetHistoryItem> Bets { get; private set; }
+        public IList<SettledBetHistoryItem> Bets { get; private set; }
 
         public int UserID
         {
             get { return Bets.Any() ? Bets.First().CustomerID : 0; }
-        } 
+        }
+
+        public bool HasUnusualWinningRate
+        {
+            get
+            {
+                return Bets.Any() && GetWinRate() > UnusualWinRateThreshold;
+            }
+        }
+
+        private decimal GetWinRate()
+        {
+            return Bets.Count(bi => bi.Win > 0)/(decimal) Bets.Count;
+        }
     }
 }
